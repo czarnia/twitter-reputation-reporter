5a9123a3ca4db5e489fc6b70898bb1ab8befda2b
Ana Czarnitzki <czarniana@gmail.com>
2019-05-25 13:29:40 -0300

    Updated docker-compose file

--- twitter-reputation-reporter-up.sh
+++ twitter-reputation-reporter-up.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker build -t twitter-reporter -f src/Dockerfile .
-docker-compose up
+docker build -t reporter-init -f src/init/Dockerfile .
+docker build -t reporter-filter-parser -f src/filter_parser/Dockerfile .
+docker build -t reporter-analyzer -f src/analyzer/Dockerfile .
+docker build -t reporter-user-reducer -f src/user_reducer/Dockerfile .
+docker build -t reporter-date-reducer -f src/date_reducer/Dockerfile .
+docker build -t date-agreggator -f src/date_agregator/Dockerfile .
+
+docker-compose up -d reporter-init reporter-filter-parser reporter-analyzer reporter-user-reducer reporter-date-reducer reporter-date-agreggator
