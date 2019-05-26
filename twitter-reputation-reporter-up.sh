5d95831805b8fc1b8ec855275c5de3d37068f860
Ana Czarnitzki <czarniana@gmail.com>
2019-05-26 01:13:22 -0300

    Fix

--- twitter-reputation-reporter-up.sh
+++ twitter-reputation-reporter-up.sh
@@ -5,6 +5,6 @@ docker build -t reporter-filter-parser -f src/filter_parser/Dockerfile .
 docker build -t reporter-analyzer -f src/analyzer/Dockerfile .
 docker build -t reporter-user-reducer -f src/user_reducer/Dockerfile .
 docker build -t reporter-date-reducer -f src/date_reducer/Dockerfile .
-docker build -t reporter-date-aggregator -f src/date_agregator/Dockerfile .
+docker build -t reporter-date-aggregator -f src/date_aggregator/Dockerfile .
 
 docker-compose up reporter-init reporter-filter-parser reporter-analyzer reporter-user-reducer reporter-date-reducer reporter-date-aggregator
